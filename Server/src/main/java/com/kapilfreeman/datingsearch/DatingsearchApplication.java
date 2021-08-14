5c3c29858d615342a12e4941e643876a39c2efc3
Kapil Dahal <kapild@mtu.edu>
2021-08-14 11:53:29 -0500

    Initial project setup

---
+++ Server/src/main/java/com/kapilfreeman/datingsearch/DatingsearchApplication.java
@@ -0,0 +1,13 @@
+package com.kapilfreeman.datingsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatingsearchApplication.class, args);
+    }
+
+}
---
+++ Server/src/test/java/com/kapilfreeman/datingsearch/DatingsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapilfreeman.datingsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
